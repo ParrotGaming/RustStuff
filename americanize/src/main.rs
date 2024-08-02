c253438eac2559fb0c3ca39906b2a4246e7e3870
ParrotGaming <31054631+ParrotGaming@users.noreply.github.com>
2024-08-01 19:50:38 -0700

    reworked Americanize

--- americanize/src/main.rs
+++ americanize/src/main.rs
@@ -9,22 +9,27 @@ fn main() {
     let original: String = args[1].clone();
     let mut edited: String = "".to_string();
     let mut buffer: String = "".to_string();
-    for c in original.char_indices() {
-        let mut end: bool = false;
-        if c.1 == ' ' || c.1.is_ascii_punctuation() || end {
-            if c.0 == original.len()-1 && !c.1.is_ascii_punctuation() {end = true}
-            if end { buffer.push(c.1);}
+    for c in original.char_indices() { 
+        if c.1 == ' ' || c.1.is_ascii_punctuation(){
+            if buffer.contains("tea") && buffer.len() == 3 {
+                buffer = "coffee".to_string();
+            } else if buffer.contains("Tea") && buffer.len() == 3 {
+                buffer = "Coffee".to_string();
+            }
+            buffer.push(c.1);
+            edited += &buffer;
+            buffer.clear();
+        } else {
+            buffer.push(c.1);
+        }
+        if c.0 == original.len() -1 {
             if buffer.contains("tea") && buffer.len() == 3 {
                 buffer = "coffee".to_string();
             } else if buffer.contains("Tea") && buffer.len() == 3 {
                 buffer = "Coffee".to_string();
             }
             edited += &buffer;
-            if !end {edited.push(c.1);}
-            buffer.clear();
-        } else {
-            buffer.push(c.1);
         }
     }
-    println!("{}", edited);
+    println!("{}!", edited);
 }
